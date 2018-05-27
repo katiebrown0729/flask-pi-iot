13140f0b4ec4a352d3625fdd5628a7f882996e29
katiebrown0729 <katiebrown0729@yahoo.com>
2018-05-27 13:10:33 -0600

    added script to help automate testing

---
+++ pi_client/test_latest.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+echo *** getting lastest from master ****
+echo accelerometer_post.py_test
+curl -OL https://raw.githubusercontent.com/katiebrown0729/flask-pi-iot/master/pi_client/accelerometer_post_test.py > accelerometer_post_test.py
+echo accelerometer_post.py
+curl -OL https://raw.githubusercontent.com/katiebrown0729/flask-pi-iot/master/pi_client/accelerometer_post.py > accelerometer_post.py
+python accelerometer_post_test.py -v
