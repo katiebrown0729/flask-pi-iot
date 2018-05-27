3cd169b3a3d6f99d29d0089531738461a5775952
katiebrown0729 <katiebrown0729@yahoo.com>
2018-05-27 13:56:29 -0600

    Remove py files

--- pi_client/test_latest.sh
+++ pi_client/test_latest.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+echo removing all the .py files
+rm *.py
 echo *** getting lastest from master ****
 echo accelerometer_post.py_test
 curl -OL https://raw.githubusercontent.com/katiebrown0729/flask-pi-iot/master/pi_client/accelerometer_post_test.py > accelerometer_post_test.py
