1371cb4f0067592ec56a0c4d66f2752af3dffb30
katiebrown0729 <katiebrown0729@yahoo.com>
2018-08-05 12:46:14 -0600

    Added YAML stuff.

--- pi_client/config/config.py
+++ pi_client/config/config.py
@@ -1,5 +1,16 @@
+'''Code to get config info from a yaml file at a given url. Made by Katie & David, still married... unbelievably'''
+
 import yaml
 import requests
 
 class YAMLConfig():
-    def
+    def get_config_from_url(self,url):
+        response = requests.get(url)
+        if (response.status_code ==200):
+            d = yaml.load(response.text)
+        print(d)
+        return d
+
+if __name__ == '__main__':
+    YConfig = YAMLConfig()  # type: YAMLConfig
+    YConfig.get_config_from_url("https://github.com/katiebrown0729/flask-pi-iot/blob/master/pi_client/config/config.yml")
