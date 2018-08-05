2ae1049bab61384f187af433bc086356dca3b057
katiebrown0729 <katiebrown0729@yahoo.com>
2018-08-05 12:59:36 -0600

    Changed YAML to list format

--- pi_client/config/config.py
+++ pi_client/config/config.py
@@ -8,9 +8,12 @@ class YAMLConfig():
         response = requests.get(url)
         if (response.status_code ==200):
             d = yaml.load(response.text)
-        print(d)
+        else:
+            print("This file is not valid")
+        #print(d)
         return d
 
 if __name__ == '__main__':
-    YConfig = YAMLConfig()  # type: YAMLConfig
-    YConfig.get_config_from_url("https://github.com/katiebrown0729/flask-pi-iot/blob/master/pi_client/config/config.yml")
+    YConfig = YAMLConfig()
+    serverdict =YConfig.get_config_from_url("https://raw.githubusercontent.com/katiebrown0729/flask-pi-iot/master/pi_client/config/config.yml")
+    print(serverdict)
